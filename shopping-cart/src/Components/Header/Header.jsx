26bb008b4a56482c3a0d15fec66adc9ca846e461
mtalha44 <mt4458649@gmail.com>
2024-12-29 23:44:56 +0500

    First day : Navbar

---
+++ shopping-cart/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Social } from "./SocialLinks"
+
+
+export const Header = () => {
+
+    return (
+        <div className="header" id="header">
+            <div className="social">
+                <Social/>
+            </div>
+        </div>
+    )
+}
---
+++ shopping-cart/src/Components/Header/SocialLinks.jsx
@@ -0,0 +1,40 @@
+import { ImGit, ImGithub, ImGoogle, ImLinkedin2, ImLocation2, ImPhone, ImPinterest2, ImTwitch, ImWhatsapp } from "react-icons/im";
+import { LuFacebook, LuGitlab, LuInstagram, LuTwitter } from "react-icons/lu";
+import { LuMessageCircle } from "react-icons/lu";
+
+import "./Header.css"
+export const Social = () => {
+    
+    return (
+        <div className="social-tab">
+
+            <div className="social-links flex-row">
+               <a href=""><ImLocation2 /> Abc, Washington Lahore Pakistan</a>
+               <a href=""><ImPhone /> +92 3055997241</a>
+               <a href=""><ImGoogle/> support@gmail.com</a>
+            </div>
+            <div className="flex-row left-social-tab">
+
+            <div className="social-icons flex-row">
+                <a href="" className="fac-icon"><LuFacebook /></a>
+                <a href="" className="git-icon"><ImGit/></a>
+                <a href="" className="glab-icon"><LuGitlab/></a>
+                <a href="" className="msg-icon"><LuMessageCircle /></a>
+                <a href="" className="twitt-icon"><LuTwitter/></a>
+                <a href="" className="twit-icon"><ImTwitch/></a>
+                <a href="" className="pin-icon"><ImPinterest2/></a>
+                <a href="" className="wats-icon"><ImWhatsapp/></a>
+                <a href="" className="linkh-icon"><ImLinkedin2/></a>
+                <a href="" className="goog-icon"><ImGoogle /></a>
+                <a href="" className="gith-icon"><ImGithub/></a>
+                <a href="" className="inst-icon"><LuInstagram/></a>
+            </div>
+            <div className="social-account flex-row">
+                {/* <a href="" className="my-account">My Account</a>
+                <a href="" className="login">Login</a> */}
+                <ImTwitch/>
+            </div>
+            </div>
+        </div>
+    )       
+}
